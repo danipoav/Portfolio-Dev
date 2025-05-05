6f7cdda8779fab6aceca6a3b28a893ed392b5ec6
danipoav <dapoav2002@gmail.com>
2025-05-05 21:12:32 +0200

    Adding hover effect to link icons

--- src/components/LeftSide.tsx
+++ src/components/LeftSide.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
@@ -9,6 +10,16 @@ import { FaGoogle } from "react-icons/fa";
 
 
 export default function LeftSide() {
+
+    const [hovered, setHovered] = useState<number | null>(null);
+
+    const icons = [
+        { icon: <FaGithub />, name: "github" },
+        { icon: <FaLinkedin />, name: "linkedin" },
+        { icon: <FaInstagram />, name: "instagram" },
+        { icon: <FaGoogle />, name: "google" },
+    ];
+
     return (
         <div className=" pl-85 py-18 ">
             <h1 className=" text-6xl font-bold">Daniel Poveda</h1>
@@ -36,10 +47,21 @@ export default function LeftSide() {
 
 
             <div className="mt-60 flex gap-4 text-3xl">
-                <FaGithub className=" cursor-pointer" />
-                <FaLinkedin className=" cursor-pointer" />
-                <FaInstagram className=" cursor-pointer" />
-                <FaGoogle className=" cursor-pointer" />
+                {icons.map((item, index) => (
+                    <div
+                        key={index}
+                        onMouseEnter={() => setHovered(index)}
+                        onMouseLeave={() => setHovered(null)}
+                        className={`cursor-pointer transition-transform duration-300 ${hovered === index
+                                ? "scale-125 text-white"
+                                : hovered !== null
+                                    ? "scale-90"
+                                    : "scale-100"
+                            }`}
+                    >
+                        {item.icon}
+                    </div>
+                ))}
             </div>
         </div>
     )
