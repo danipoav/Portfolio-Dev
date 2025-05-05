c2912925ac0acf1044ab87c92dca237a902dc358
danipoav <dapoav2002@gmail.com>
2025-05-05 17:26:05 +0200

    Changing light colors

--- src/components/CursorLight.tsx
+++ src/components/CursorLight.tsx
@@ -1,27 +1,27 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-export default function CursorLight({ children }: { children: React.ReactNode }) {
-
+export default function SpotlightLayout({ children }: { children: React.ReactNode }) {
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
     const handleMouseMove = (e: MouseEvent) => {
       setPosition({ x: e.clientX, y: e.clientY });
-    }
+    };
     window.addEventListener("mousemove", handleMouseMove);
     return () => window.removeEventListener("mousemove", handleMouseMove);
-  }, [])
+  }, []);
 
   return (
-    <div className="relative w-full h-full overflow-hidden">
+    <div className="relative w-full h-full bg-gray-800 overflow-hidden">
+      {/* Spotlight */}
       <div
         className="pointer-events-none fixed top-0 left-0 w-full h-full z-50"
         style={{
-          background: `radial-gradient(150px at ${position.x}px ${position.y}px, rgba(255,255,255,0.05), rgba(0,0,0,0.9))`,
+          background: `radial-gradient(500px at ${position.x}px ${position.y}px, rgba(255, 255, 255, 0.233), transparent)`,
           mixBlendMode: "soft-light",
         }}
       />
       {children}
     </div>
-  )
+  );
 }
--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,15 +1,18 @@
+import CursorLight from "../components/CursorLight";
 import LeftSide from "../components/LeftSide";
 import RightSide from "../components/RightSide";
 
 export default function MainLayout() {
     return (
-        <div className=" flex h-screen">
-            <div className=" w-1/2 fixed left-0 top-0 h-screen bg-gray-800 text-white p-6">
-                <LeftSide />
+        <CursorLight>
+            <div className=" flex h-screen">
+                <div className=" w-1/2 fixed left-0 top-0 h-screen bg-gray-800 text-white p-6">
+                    <LeftSide />
+                </div>
+                <div className=" w-1/2 ml-auto h-screen overflow-y-scroll p-6 bg-gray-800 text-white">
+                    <RightSide />
+                </div>
             </div>
-            <div className=" w-1/2 ml-auto h-screen overflow-y-scroll p-6 bg-gray-800 text-white">
-                <RightSide />
-            </div>
-        </div>
+        </CursorLight>
     )
 }
