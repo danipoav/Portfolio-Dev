c2e997b049a709c7cf7aed12fc61dc64f0988b8a
danipoav <dapoav2002@gmail.com>
2025-05-05 16:18:14 +0200

    Crating light that follows cursor pointer

---
+++ src/components/CursorLight.tsx
@@ -0,0 +1,6 @@
+
+export default function CursorLight() {
+  return (
+    <div>CursorLight</div>
+  )
+}
--- src/components/LeftSide.tsx
+++ src/components/LeftSide.tsx
@@ -1,3 +1,12 @@
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaGoogle } from "react-icons/fa";
+
+
+
+
+
 
 export default function LeftSide() {
     return (
@@ -8,14 +17,29 @@ export default function LeftSide() {
 
             <h3 className=" text-lg pt-3 text-gray-400 pr-50">I build accessible, pixel-perfect digital experiences for the web.</h3>
 
-            <div className=" py-18">
+            <div className="py-18">
+                <ul className="flex flex-col uppercase text-sm font-bold text-gray-400">
+                    <li className="relative inline-block py-2 cursor-pointer hover:text-white transition-all duration-300 ease-in-out hover:translate-x-1">
+                        <span className="after:content-[''] after:block after:h-[2px] after:w-0 after:bg-white after:transition-all after:duration-300 hover:after:w-full">About</span>
 
-                <ul className="flex flex-col uppercase text-sm font-bold text-gray-400 w-200">
-                    <li className="inline-block  py-2 hover:text-white cursor-pointer w-auto">About</li>
-                    <li className="inline-block  py-2 hover:text-white cursor-pointer w-auto">Experience</li>
-                    <li className="inline-block py-2 hover:text-white cursor-pointer w-auto">Projects</li>
+                    </li>
+                    <li className="relative inline-block py-2 cursor-pointer hover:text-white transition-all duration-300 ease-in-out hover:translate-x-1">
+                        <span className="after:content-[''] after:block after:h-[2px] after:w-0 after:bg-white after:transition-all after:duration-300 hover:after:w-full">Experience</span>
+
+                    </li>
+                    <li className="relative inline-block py-2 cursor-pointer hover:text-white transition-all duration-300 ease-in-out hover:translate-x-1">
+                        <span className="after:content-[''] after:block after:h-[2px] after:w-0 after:bg-white after:transition-all after:duration-300 hover:after:w-full">Projects</span>
+
+                    </li>
                 </ul>
+            </div>
 
+
+            <div className="mt-60 flex gap-4 text-3xl">
+                <FaGithub className=" cursor-pointer" />
+                <FaLinkedin className=" cursor-pointer" />
+                <FaInstagram className=" cursor-pointer" />
+                <FaGoogle className=" cursor-pointer" />
             </div>
         </div>
     )
