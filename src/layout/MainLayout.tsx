f2d7885d591cb52b6cfb5e6d53f5e38cac32708b
danipoav <dapoav2002@gmail.com>
2025-05-06 15:11:45 +0200

    Removing some hovers

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -7,11 +7,11 @@ export default function MainLayout() {
         <CursorLight>
             <div className=" flex h-screen ">
                 <div className=" w-1/2 fixed left-0 top-0 h-screen bg-gray-800 text-white p-6">
-                <div className=" fixed bottom-0 left-20 border border-gray-400 h-25 hover:border-white hover:scale-120 transition-transform duration-300"></div>
+                <div className=" fixed bottom-0 left-20 border border-white h-25 "></div>
                     <LeftSide />
                 </div>
                 <div className=" w-1/2 ml-auto h-screen overflow-y-scroll p-6 bg-gray-800 text-white">
-                <div className=" fixed bottom-0 right-20 border border-gray-400 h-25 hover:border-white hover:scale-120 transition-transform duration-300"></div>
+                <div className=" fixed bottom-0 right-20 border border-white h-25"></div>
                     <RightSide />
                 </div>
             </div>
