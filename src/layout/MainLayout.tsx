06b4396556f75821f106adb88ee98a82141ece2d
danipoav <dapoav2002@gmail.com>
2025-05-12 23:00:42 +0200

    Removing somre colors and comments

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -45,11 +45,11 @@ export default function MainLayout() {
     return (
         <CursorLight>
             <div className="flex h-screen">
-                <div className="w-1/2 fixed left-0 top-0 h-screen bg-gray-800 text-white p-6">
+                <div className="w-1/2 fixed left-0 top-0 h-screen bg-gray-900 text-white p-6">
                     <div className="fixed bottom-0 left-20 border border-white h-25" />
                     <LeftSide />
                 </div>
-                <div className="w-1/2 ml-auto h-screen overflow-y-scroll p-6 bg-gray-800 text-white">
+                <div className="w-1/2 ml-auto h-screen overflow-y-scroll p-6 bg-gray-900 text-white">
                     <div className="fixed bottom-0 right-20 border border-white h-25" />
                     <RightSide />
                 </div>
