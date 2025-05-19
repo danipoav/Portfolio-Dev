74bf6c2b9c32ebd012f3c76d5270d3d6a97b7f15
danipoav <dapoav2002@gmail.com>
2025-05-19 13:42:41 +0200

    Adding mi real edxperience content

--- src/sections/ExperienceCard.tsx
+++ src/sections/ExperienceCard.tsx
@@ -1,47 +1,26 @@
 
-export default function ExperienceCard() {
+interface ExperienceProps {
+    title: string,
+    description: string,
+    date: string,
+    techStack: string[],
+}
+
+export default function ExperienceCard({ title, techStack = [], date, description }: ExperienceProps) {
     return (<>
-        <div className=" flex hover:rounded-2xl hover:bg-gray-800 px-5 py-3 mb-10">
-            <h2 className=" text-gray-600 w-[60%] text-sm">JULY - DEC 2017</h2>
+        <div className=" flex hover:rounded-2xl hover:bg-gray-800 px-5 py-3 mb-10 cursor-pointer">
+            <h2 className=" text-gray-600  pr-10 text-sm">{date}</h2>
             <div>
-                <h1 className=" text-white mb-3 text-lg">DIBA - EnterShip - Road Section</h1>
-                <p className=" text-gray-600 text-sm mb-3">Developed and styled interactive web apps for Apple Music, including the user interface of Apple Music`s embeddabe</p>
+                <h1 className=" text-white mb-3 text-lg ">{title}</h1>
+                <p className=" text-gray-600 text-sm mb-3">{description}</p>
                 <ul className="flex gap-1">
-                    <li className=" rounded-2xl bg-blue-900 text-blue-300 text-xs py-2 px-3">Ember</li>
-                    <li className=" rounded-2xl bg-blue-900 text-blue-300 text-xs py-2 px-3">SCSS</li>
-                    <li className=" rounded-2xl bg-blue-900 text-blue-300 text-xs py-2 px-3">JavaScript</li>
-                    <li className=" rounded-2xl bg-blue-900 text-blue-300 text-xs py-2 px-3">WordPress</li>
+                    {techStack.map((tech, i) => (
+                        <li key={i} className="rounded-2xl bg-blue-900 text-blue-300 text-xs py-2 px-3">{tech}</li>
+                    ))}
+                    
                 </ul>
             </div>
         </div>
-
-        <div className=" flex hover:rounded-2xl hover:bg-gray-800 px-5 py-3 mb-10">
-            <h2 className=" text-gray-600 w-[60%] text-sm">JULY - DEC 2017</h2>
-            <div>
-                <h1 className=" text-white mb-3 text-lg">DIBA - EnterShip - Road Section</h1>
-                <p className=" text-gray-600 text-sm mb-3">Developed and styled interactive web apps for Apple Music, including the user interface of Apple Music`s embeddabe</p>
-                <ul className="flex gap-1">
-                    <li className=" rounded-2xl bg-blue-900 text-blue-300 text-xs py-2 px-3">Ember</li>
-                    <li className=" rounded-2xl bg-blue-900 text-blue-300 text-xs py-2 px-3">SCSS</li>
-                    <li className=" rounded-2xl bg-blue-900 text-blue-300 text-xs py-2 px-3">JavaScript</li>
-                    <li className=" rounded-2xl bg-blue-900 text-blue-300 text-xs py-2 px-3">WordPress</li>
-                </ul>
-            </div>
-        </div>
-
-        <div className=" flex hover:rounded-2xl hover:bg-gray-800 px-5 py-3 mb-10">
-            <h2 className=" text-gray-600 w-[60%] text-sm">JULY - DEC 2017</h2>
-            <div>
-                <h1 className=" text-white mb-3 text-lg">DIBA - EnterShip - Road Section</h1>
-                <p className=" text-gray-600 text-sm mb-3">Developed and styled interactive web apps for Apple Music, including the user interface of Apple Music`s embeddabe</p>
-                <ul className="flex gap-1">
-                    <li className=" rounded-2xl bg-blue-900 text-blue-300 text-xs py-2 px-3">Ember</li>
-                    <li className=" rounded-2xl bg-blue-900 text-blue-300 text-xs py-2 px-3">SCSS</li>
-                    <li className=" rounded-2xl bg-blue-900 text-blue-300 text-xs py-2 px-3">JavaScript</li>
-                    <li className=" rounded-2xl bg-blue-900 text-blue-300 text-xs py-2 px-3">WordPress</li>
-                </ul>
-            </div>
-        </div>
-        </>
+    </>
     )
 }
--- src/sections/ExperienceContent.tsx
+++ src/sections/ExperienceContent.tsx
@@ -1,7 +1,27 @@
 import ExperienceCard from './ExperienceCard'
 
 export default function ExperienceContent() {
+
+    const experience = [
+        {
+            title: "DIBA - Diputació de Barcelona - Road Section",
+            description: "During my stay in DIBA i participated in the design and development of institutional websites, focusing on HTML, CSS and TypeScript layout structuring, Gained hands-on experience working within a public sector environment, collaborating with professionals and following accessibility and usability guidelines and improved front-end development skills and understanding of web standards in real-world projects",
+            date: "2022-2023",
+            techStack: ["HTML", "Tailwind CSS", "TypeScript", "MySQL"]
+        },
+        {
+            title: "Oxygen Workspace",
+            description: "At OxygenShop, I worked as a Full Stack Intern, contributing to both front-end and back-end development. I helped build responsive interfaces and supported API integration, gaining hands-on experience with real-world web applications and modern development practices.",
+            date: "2022-2023",
+            techStack: ["PHP", "React", "NODE.js", "MongoDB", "Laravel"]
+        }
+    ]
+
     return (
-        <ExperienceCard />
+        <section id='experience' className='scroll-mt-10 mb-20'>
+            {experience.map((exp, i) => (
+                <ExperienceCard key={i} {...exp} />
+            ))}
+        </section>
     )
 }
